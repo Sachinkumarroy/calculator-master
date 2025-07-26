c72831df6e18c7899d094bd59e7a851a817f2dd7
Sachinkumarroy <132121701+Sachinkumarroy@users.noreply.github.com>
2025-07-26 14:22:07 +0530

    Updates

---
+++ precache-manifest.7d3aa8cc3c839e79466b6f9c464c9e42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64b8d712279457fe62c67dc9ecae9687",
+    "url": "/calculator-master/index.html"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator-master/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "730d09b7d06a58d03369",
+    "url": "/calculator-master/static/css/main.6fdcdd19.chunk.css"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator-master/static/js/2.9067ee69.chunk.js"
+  },
+  {
+    "revision": "730d09b7d06a58d03369",
+    "url": "/calculator-master/static/js/main.6ccdbe39.chunk.js"
+  },
+  {
+    "revision": "0285ecf2801524fc8bba",
+    "url": "/calculator-master/static/js/runtime~main.15eb0c05.js"
+  }
+]);
